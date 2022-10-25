6a295db26f07dd9efccb22a28956dbc0401cd930
likhonalmamun <alikhon055@gmail.com>
2022-10-25 11:13:51 +0600

    hosted to  vercel and added templete code

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hello')
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+app.use(cors());
+app.get("/", (req, res) => {
+  res.json("Hello World!");
+});
+app.get("/data", (req, res) => {
+  res.json("this is the data sent from the server");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
