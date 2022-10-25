aebb6ffd791496d8ef689ba5b880d2045a6c78e9
likhonalmamun <alikhon055@gmail.com>
2022-10-25 12:44:12 +0600

    added data about subjects

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 app.use(cors());
+
+const allSubject = require("./subjects.json");
 app.get("/", (req, res) => {
-  res.json("Hello World!");
+  res.send(JSON.stringify(allSubject));
 });
 app.get("/data", (req, res) => {
   res.json("this is the data sent from the server");
@@ -13,3 +15,4 @@ app.get("/data", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+console.log(allSubject);
